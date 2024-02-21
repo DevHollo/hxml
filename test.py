bf01b6a25bff252b8653c10a651c540366213342
Hollo <127456155+DevHollo@users.noreply.github.com>
2024-02-20 21:09:07 -0700

    Update and rename test.py to epic_xml.py

--- test.py
+++
@@ -1,6 +0,0 @@
-from hxml import XML
-
-xml = XML("test.xml")
-
-epic_xml = xml.get("le_epic_xml")
-print(epic_xml) # output: {'name': 'Epic XML', 'cool': {'a': 'as', 'b': 'you', 'c': 'see', 'd': 'it', 'e': 'works!'}}
