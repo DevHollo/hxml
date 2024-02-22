576c110f34fef9eef59687b6a159ae4774807414
Hollo <127456155+DevHollo@users.noreply.github.com>
2024-02-21 18:45:04 -0700

    Update hxml.py

--- hxml.py
+++ hxml.py
@@ -1,14 +1,29 @@
 class XML:
     def __init__(self, xml_file: str) -> None:
+        from importlib import metadata as pypi
         import xml.etree.ElementTree as ET
-        from sty import fg, Style, RgbFg
+        import subprocess
+        import requests
+        import logging
         self.xml = xml_file
         self.tree = ET.parse(xml_file)
         self.root = self.tree.getroot()
-        fg.custom = Style(RgbFg(8, 214, 42))
-        print(fg.custom + "Hello from hxml!" + fg.rs)
+        try:
+            user_ver = str(pypi.version('hxml'))
+            pypi_ver = str(requests.get("https://pypi.org/pypi/hxml/json").json()['info']['version'])
+            if not user_ver == pypi_ver:
+                subprocess.run(['pip', 'install', f'hxml=={pypi_ver}'])
+            else:
+                pass
+        except pypi.PackageNotFoundError as e:
+            logging.error(f"Package hxml not found: {e}")
+        except requests.RequestException as e:
+            logging.error(f"Failed to retrieve version from PyPI for hxml: {e}")
+        except subprocess.CalledProcessError as e:
+            logging.error(f"Failed to install hxml package: {e}")
 
-    def get(self, item: str):
+    def get_root_item(self, item: str):
+        import hollos_get_data_recursive
         root_element_name = self.root.tag
         if item != root_element_name:
             print(f"Error: Provided item '{item}' does not match root element '{root_element_name}'.")
@@ -16,15 +31,6 @@ class XML:
         
         data = {}
         for child in self.root:
-            data[child.tag] = self._get_data_recursive(child)
+            data[child.tag] = hollos_get_data_recursive.init()._get_data_recursive(child)
         
         return data
-
-    def _get_data_recursive(self, element):
-        if len(element) == 0:
-            return element.text.strip() if element.text else None
-        else:
-            data = {}
-            for child in element:
-                data[child.tag] = self._get_data_recursive(child)
-            return data
