d9982c50431a31327d4153ff129cc31fc160977a
Hollo <127456155+DevHollo@users.noreply.github.com>
2024-02-21 18:39:44 -0700

    Update epic_xml.py

--- epic_xml.py
+++ epic_xml.py
@@ -2,5 +2,5 @@ from hxml import XML
 
 xml = XML("test.xml")
 
-epic_xml = xml.get("le_epic_xml")
+epic_xml = xml.get_root_item("le_epic_xml")
 print(epic_xml) # output: {'name': 'Epic XML', 'cool': {'a': 'as', 'b': 'you', 'c': 'see', 'd': 'it', 'e': 'works!'}}
