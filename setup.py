2d1c96a7475efeacd3451a00c7bd4300166be788
Hollo <127456155+DevHollo@users.noreply.github.com>
2024-02-21 18:44:07 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open('README.md', 'r') as mdfile:
 
 setup(
     name='hxml',
-    version='0.0.1',
+    version='0.0.2',
     scripts=['hxml.py'],
     author="Hollo",
     author_email="hollo1234567890e@gmail.com",
     long_description=des,
     long_description_content_type="text/markdown",
     install_requires=[
-        "sty==1.0.6"
+        "hollos-get-data-recursive==0.0.1"
     ],
     url="https://github.com/DevHollo/hxml"
 )
